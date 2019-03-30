4b8a229c05cddf91f8f5bab1f5e09f42a1f94301
vagrant <vagrant@homestead>
2019-03-30 17:04:40 +0000

    UI

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="footer">
   <img class="brand-icon" src="https://iocaffcdn.phphub.org/uploads/sites/KDiyAbV0hj1ytHpRTOlVpucbLebonxeX.png">
-  <a href="https://learnku.com/laravel/courses" target=_blank>
+  <a href="#" target=_blank>
     讯捷科技有限公司
   </a>
 
